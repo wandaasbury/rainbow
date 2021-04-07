c3420cd47d83c0e7f0d4808046b67ce8ad133347
Wanda Asbury <shawanda.asbury@gmail.com>
2021-04-07 13:46:08 -0400

    Using innerHTML and style display property

---
+++ Rainbow.js
@@ -0,0 +1,37 @@
+
+//Div text 
+
+document.getElementById('red').innerHTML = 'RED';
+document.getElementById('orange').innerHTML = 'ORANGE';
+document.getElementById('yellow').innerHTML = 'YELLOW';
+document.getElementById('green').innerHTML = 'GREEN';
+document.getElementById('indigo').innerHTML = 'INDIGO';
+document.getElementById('violet').innerHTML = 'VIOLET';
+
+
+
+//div color
+document.getElementById('red').style.backgroundColor = "red";
+document.getElementById('orange').style.backgroundColor = "orange";
+document.getElementById('yellow').style.backgroundColor = "yellow";
+document.getElementById('green').style.backgroundColor = "green";
+document.getElementById('indigo').style.backgroundColor = "indigo";
+document.getElementById('violet').style.backgroundColor = "violet";
+
+var div = document.getElementsByTagName('div');
+
+var divTextAlign = div[0].style.textAlign = "center";
+var divTextAlign = div[1].style.textAlign = "center";
+var divTextAlign = div[2].style.textAlign = "center";
+var divTextAlign = div[3].style.textAlign = "center";
+var divTextAlign = div[4].style.textAlign = "center";
+var divTextAlign = div[5].style.textAlign = "center";
+
+//div font weight
+
+var divBoldText = div[0].style.fontWeight = "bold";
+var divBoldText = div[1].style.fontWeight = "bold";
+var divBoldText = div[2].style.fontWeight = "bold";
+var divBoldText = div[3].style.fontWeight = "bold";
+var divBoldText = div[4].style.fontWeight = "bold";
+var divBoldText = div[5].style.fontWeight = "bold";
